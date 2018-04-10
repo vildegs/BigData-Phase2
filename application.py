7d2740ec02a191adc6081062e9679ab8a28c2cbe
Vilde Stokke <vildegstokke@hotmail.com>
2018-04-10 15:57:35 +0200

    framework

--- application.py
+++ application.py
@@ -11,8 +11,20 @@ def createRDD(filename, val):
     rdd_sample = rdd.sample(False, val, 5)
     return rdd_sample
 
+def run(rdd, tweet, outputFile):
+
+# x = place, y = probability
+# Can be more than one outcome
+def writeFile(rdd, filename):
+    # If there's one or more places found
+    if rdd.count() > 0:
+        rdd.map(lambda (x,y): x + "\t" + str(y)).coalesce(1).saveAsTextFile(filename)
+    # If there's no place with prob greater than zero
+    else:
+        rdd.parallellize([]).coalesce(1).saveAsTextFile(filename)
+
 def main():
     rdd = createRDD("./data/geotweets.tsv", 0.1)
-    run("./result.tsv", rdd)
+
 
 main()
