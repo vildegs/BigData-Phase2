76cfbe1d76162ac80d4c22d49f70efcf269848a3
kaisarokne <kaisa.rokne@gmail.com>
2018-04-11 17:08:46 +0200

    changes to code

--- classify.py
+++ classify.py
@@ -6,17 +6,29 @@ rdd_sample = None
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 
-# Creates an RDD from #5 place_name and #11 tweet_text
+# Creates an RDD from values #5 place_name and #11 tweet_text
 def createRDD(filename, val):
-    rdd = sc.textFile(filename).map(lambda line: line.split('\t')).map(lambda x: x[4], x[10].lower().split(" ")))
+    rdd = sc.textFile(filename, use_unicode=True).map(lambda line: line.split('\t')).map(lambda x: (x[4], x[10].lower().split(" ")))
     rdd_sample = rdd.sample(False, val, 5)
     return rdd_sample
+
+def transformInputTweet(inputTweet):
+    tweet = inputTweet.lower().split(" ")
+    return tweet
 
 # We need three parameters:
 # -training <full path of the training file>
 # -input <full path of the input file>
 # -output <full path of the output file>
 def run(rdd, tweet, outputFile):
+    return 1
+
+def naiveBayes(rdd, place, tweet):
+    T = rdd.count()
+    TcRDD = rdd.filter(lambda x: (x[4] == place))
+    Tc = TcRDD.count()
+    #Tcw = TcRDD.filter(lambda x: x[10] if (x[10].find(word) for word in tweet))
+
 
 # x = place, y = probability
 # Can be more than one outcome
