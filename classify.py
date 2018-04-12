18384423d6f05f4ea91d863fbbc0ee5695b950cb
Vilde Stokke <vildegstokke@hotmail.com>
2018-04-12 13:14:14 +0200

    made functionality for most parts

--- classify.py
+++ classify.py
@@ -15,40 +15,66 @@ rdd_sample = None
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
+sc.setLogLevel("OFF")
 
 # Creates an RDD from values #5 place_name and #11 tweet_text
 def createRDD(val):
+    parser = argparse.ArgumentParser(description = "Classification using Naive Bayes.")
+
+    parser.add_argument('-training', '-t', dest = 'training', default = './data/geotweets.tsv')
+    parser.add_argument('-input', '-i', dest = 'inputTweet')
+    parser.add_argument('-output', '-o', dest = 'outputFile', default = './outputFile.tsv')
+
+    args = parser.parse_args()
     rdd = sc.textFile(args.training, use_unicode = True)\
             .map(lambda line: line.split('\t')).map(lambda x: (x[4], x[10].lower().split(" ")))
     rdd_sample = rdd.sample(False, val, 5)
-    return rdd_sample
+    rddCount = rdd_sample.count()
+    return rdd_sample, rddCount
 
+
+# ((place, word), count) - sorted by descending order
 def countRDD(rdd):
-    rddMapped = rdd.flatMap(lambda x: ((x[0], word) for word in x[1])) \
-                    .map(lambda x: ((x[0], x[1], 1))) \
-                    .reduceByKey(lambda x,y, x+y) \
-                    .sortBy(lambda x: x[1], False)
+    rddWordCount = rdd.flatMap(lambda x: ((x[0], word) for word in x[1])) \
+                    .map(lambda x: ((x[0], x[1]), 1)) \
+                    .reduceByKey(lambda x,y: x+y) \
+                    .sortBy(lambda x: x[1], ascending = False)
+    return rddWordCount
+
 
 def transformInputTweet(tweetText):
-    tweet = tweetText.lower().split(" ")
+    #tweet = tweetText.lower().split(" ")
     #tweetTrans = sc.textFile(args.inputTweet, use_unicode = True)
     return ["hello", "world"]
 
-# We need three parameters:
-# -training <full path of the training file>
-# -input <full path of the input file>
-# -output <full path of the output file>
-def run(rdd, tweet, outputFile):
-    return 1
 
-def naiveBayes(rdd, place, tweet):
-    T = rdd.count()
-    TcRDD = rdd.filter(lambda x: (x[4] == place))
-    Tc = TcRDD.count()
+def wordCount(rdd, inputWord):
+    rddTweetWord = rdd.filter(lambda x: x[0][1] == inputWord)
+    return rddTweetWord.count()
+
+
+def naiveBayes(rdd, inputTweet, rddCount):
+    T = rddCount
+    Tc = rdd.count()
+    #Tc = TcRDD.count()
     #Tcw = TcRDD.filter(lambda x: x[10] if (x[10].find(word) for word in tweet))
-    for word in tweet:
+    prob = float(T/Tc)
+    for word in inputTweet:
+        Tcw = wordCount(rdd, word)
+        prob *= float(Tcw/Tc)
+    return prob
 
-
+def calculateProbabilities(rdd, inputTweet, rddCount):
+    places = rdd.map(lambda x: x[0]).distinct().collect()
+    probabilities = []
+    rddWordCount = countRDD(rdd)
+    for place in places:
+        rddPlace = rddWordCount.filter(lambda x: x[0][0] == place)
+        prob = naiveBayes(rddPlace, inputTweet, rddCount)
+        probabilities.append(prob)
+        print(place, ",", prob)
+    maxProb = sc.parallellize(probabilities).max(key =lambda x: x[1])
+    return maxProb
 
 # x = place, y = probability
 # Can be more than one outcome
@@ -58,11 +84,14 @@ def writeFile(rdd):
         rdd.map(lambda (x,y): x + "\t" + str(y)).coalesce(1).saveAsTextFile(args.outputFile)
     # If there's no place with prob greater than zero
     else:
-        rdd.parallellize([]).coalesce(1).saveAsTextFile(args.outputFile)
+        results = sc.parallellize([])
+        results.coalesce(1).saveAsTextFile(args.outputFile)
 
 def main():
-    training_rdd = createRDD(0.1)
-    tweet = transformInputTweet(args.inputTweet)
+    training_rdd, rddCount = createRDD(0.1)
+    inputTweet = transformInputTweet(args.inputTweet)
+    #rddWordCount = countRDD(training_rdd)
+    calculateProbabilities(training_rdd, inputTweet, rddCount)
 
 
 
